0e72ed6767cfe7a8a1129b94575c2234241e3107
kalburgimanjunath <1643278+kalburgimanjunath@users.noreply.github.com>
2023-09-07 20:47:23 +0530

    1 file modified

--- pages/profilecard.js
+++ pages/profilecard.js
@@ -4,10 +4,13 @@ export default function Profilecard() {
   const Sidebar = () => {
     return (
       <div className="shadow-lg p-5 m-5 border">
-        <h1>Theme</h1>
-        <p>Use the below fields to fully customize the styling of your page</p>
-        <lagend>Page Styles</lagend>
+        <h1 className="font-bold text-2xl">Theme</h1>
+        <p className="text-xs">
+          Use the below fields to fully customize the styling of your page
+        </p>
+
         <form>
+          <lagend>Page Styles</lagend>
           <div>
             <div>Background color </div>
             <div>
@@ -20,14 +23,78 @@ export default function Profilecard() {
               <Input />
             </div>
           </div>
+          <lagend>Profile Styles</lagend>
+          <div>
+            <div>Styles</div>
+            <div>
+              <Input />
+            </div>
+          </div>
+          <div>
+            <div>Background Image Styles</div>
+            <div>
+              <Input />
+            </div>
+          </div>
+          <div>
+            <div>Background Color</div>
+            <div>
+              <Input />
+            </div>
+          </div>
+          <div>
+            <div>Name color</div>
+            <div>
+              <Input />
+            </div>
+          </div>
+          <div>
+            <div>Description color</div>
+            <div>
+              <Input />
+            </div>
+          </div>
         </form>
       </div>
     );
   };
   const ProfileCard = () => {
     return (
-      <div className="shadow-lg p-5 m-5 border bg-pink-100">
-        <div className="shadow-lg p-5 m-5 border bg-white">Profile Card</div>
+      <div className="shadow-lg border rounded-lg bg-pink-100 p-5">
+        <div>
+          <img
+            src="https://picsum.photos/id/237/200/300"
+            className="w-full h-28"
+          />
+        </div>
+        <div className="shadow-lg border rounded-md bg-white p-5">
+          <div className="flex justify-between">
+            <div>
+              <img
+                src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50"
+                width="50"
+                height="50"
+              />
+            </div>
+            <div>
+              <button
+                type="button"
+                className="bg-black text-white rounded-md border mt-2 p-1"
+              >
+                Share
+              </button>
+            </div>
+          </div>
+          <div className="">
+            <div className="mt-5">
+              <div className="font-bold">Manjunath Kalburgi</div>
+            </div>
+            <div>
+              Lorem text Lorem text Lorem textLorem textLorem text Lorem text
+              Lorem text Lorem textLorem text Lorem textLorem text
+            </div>
+          </div>
+        </div>
       </div>
     );
   };
